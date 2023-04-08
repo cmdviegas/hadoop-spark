9866c988b5225375b86b670cb542dcabe980e7b5
Carlos Viegas <cmdviegas@users.noreply.github.com>
2023-04-08 09:35:59 -0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -13,14 +13,20 @@
 # This is a bash script to automatically download hadoop and spark 
 #
 
-read -p "Deseja fazer o download do Apache Hadoop 3.3.5 (yes/NO): " option
+read -p "Would you like to download Hadoop and Spark from Apache servers? (yes/NO): " option
 
 if [[ "$option" =~ ^[Yy][Ee][Ss]$ ]]; then
-  wget --no-check-certificate https://dlcdn.apache.org/hadoop/common/hadoop-3.3.5/hadoop-3.3.5.tar.gz
-fi
 
-read -p "Deseja fazer o download do Apache Spark 3.3.2 (yes/NO): " option
+  read -p "Download Apache Hadoop 3.3.5? (yes/NO): " hadoop
 
-if [[ "$option" =~ ^[Yy][Ee][Ss]$ ]]; then
-  wget --no-check-certificate https://dlcdn.apache.org/spark/spark-3.3.2/spark-3.3.2-bin-hadoop3.tgz
+  read -p "Download Apache Spark 3.3.2 (yes/NO): " spark
+
+  if [[ "$hadoop" =~ ^[Yy][Ee][Ss]$ ]]; then
+    wget --no-check-certificate https://dlcdn.apache.org/hadoop/common/hadoop-3.3.5/hadoop-3.3.5.tar.gz
+  fi
+
+  if [[ "$spark" =~ ^[Yy][Ee][Ss]$ ]]; then
+    wget --no-check-certificate https://dlcdn.apache.org/spark/spark-3.3.2/spark-3.3.2-bin-hadoop3.tgz
+  fi
+
 fi
