bf6616d3dcd4d78ed06d77f06f2b1be3dfe9e05a
Carlos Viegas <cmdviegas@users.noreply.github.com>
2023-04-08 00:11:19 -0300

    Update bootstrap.sh

--- config_files/bootstrap.sh
+++ config_files/bootstrap.sh
@@ -13,6 +13,9 @@
 # This is a bash script that initialize Hadoop services
 #
 
+USERNAME=$1
+PASSWORD=$2
+
 # Some coloring
 green=$(tput setaf 2)
 lblue=$(tput setaf 6)
@@ -20,16 +23,24 @@ reset=$(tput sgr0)
 printf " * [${green}BOOT${reset}] ${lblue}Starting bootstrap.sh${reset}...\n"
 
 # Load .bashrc
-source /root/.bashrc
+printf " * [${green}BASH${reset}] ${lblue}Loading .bashrc${reset}...\n"
+
+echo "export HDFS_NAMENODE_USER=\"$1\"" >> ~/.bashrc
+#source ${MYDIR}/.bashrc
+eval "$(tail -n +10 ~/.bashrc)" # workaround for ubuntu .bashrc
 
 # Copy hosts to hosts file
-cat /root/config_files/hosts >> /etc/hosts
+printf " * [${green}HOSTS${reset}] ${lblue}Copying hosts file${reset}...\n"
+echo "${PASSWORD}" | sudo -S bash -c "cat ${PWD}/config_files/hosts > /etc/hosts"
 
 # Start SSH service
-service ssh start
+echo "${PASSWORD}" | sudo -S service ssh start
 
 # Do only at node-master
 if [ "$HOSTNAME" == "node-master" ] ; then
+
+    # Install python packages for pyspark (OPTIONAL)
+    #pip install --upgrade pip && pip install pandas pyarrow
 
     # Hadoop banner
     printf "
@@ -43,10 +54,10 @@ if [ "$HOSTNAME" == "node-master" ] ; then
     ${green} ▒ ░▒░ ░  ▒   ▒▒ ░ ░ ▒  ▒   ░ ▒ ▒░   ░ ▒ ▒░ ░▒ ░
     ${green} ░  ░░ ░  ░   ▒    ░ ░  ░ ░ ░ ░ ▒  ░ ░ ░ ▒  ░░
     ${green} ░  ░  ░      ░  ░   ░        ░ ░      ░ ░     ${lblue}$(tput blink)v3.3.5${reset}\n\n"
-
+                                        
     # Time to wait for slaves to establish ssh connection with node-master
     # Only needed when node-master is started before slaves
-    sleep 8
+    sleep 6
 
     # Format HDFS only at first boot
     FILE=.first_boot
@@ -69,8 +80,8 @@ if [ "$HOSTNAME" == "node-master" ] ; then
 
     sleep 5
 
-    # Create /user, /user/root, /spark-logs and /spark-libs folders
-    hdfs dfs -mkdir -p /user/root
+    # Create /user folders
+    hdfs dfs -mkdir -p /user/${HDFS_NAMENODE_USER}
     hdfs dfs -mkdir /spark-logs
     hdfs dfs -mkdir /spark-libs
     hdfs dfs -put ${SPARK_HOME}/jars/*.jar /spark-libs/
@@ -78,6 +89,30 @@ if [ "$HOSTNAME" == "node-master" ] ; then
     # Start SPARK history server
     printf " * [${green}SPARK${reset}] ${lblue}Starting SPARK history server${reset}...\n"
     start-history-server.sh
+
+    printf "
+    ${lblue} APACHE
+    ${green}   ██████  ██▓███   ▄▄▄       ██▀███   ██ ▄█▀
+    ${green} ▒██    ▒ ▓██░  ██▒▒████▄    ▓██ ▒ ██▒ ██▄█▒ 
+    ${green} ░ ▓██▄   ▓██░ ██▓▒▒██  ▀█▄  ▓██ ░▄█ ▒▓███▄░ 
+    ${green}   ▒   ██▒▒██▄█▓▒ ▒░██▄▄▄▄██ ▒██▀▀█▄  ▓██ █▄ 
+    ${green} ▒██████▒▒▒██▒ ░  ░ ▓█   ▓██▒░██▓ ▒██▒▒██▒ █▄
+    ${green} ▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░ ▒▒   ▓▒█░░ ▒▓ ░▒▓░▒ ▒▒ ▓▒
+    ${green} ░ ░▒  ░ ░░▒ ░       ▒   ▒▒ ░  ░▒ ░ ▒░░ ░▒ ▒░
+    ${green} ░  ░  ░  ░░         ░   ▒     ░░   ░ ░ ░░ ░ 
+    ${green}       ░                 ░  ░   ░     ░  ░       ${lblue}$(tput blink)v3.3.2${reset}\n\n"
+
+    sleep 5
+
+    printf " * [${green}HDFS${reset}] ${lblue}Checking HDFS nodes report${reset}...\n"
+    hdfs dfsadmin -report
+
+    sleep 8
+
+    printf " * [${green}YARN${reset}] ${lblue}Checking YARN nodes list${reset}...\n"
+    yarn node -list
+
+    sleep 2
 
     printf " * ${green}$(tput blink)All set!${reset}\n"
 else
