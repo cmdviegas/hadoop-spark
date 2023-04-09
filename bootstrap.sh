6e42e2580ef35a31e58dbbdb50612733b2059747
Carlos Viegas <cmdviegas@users.noreply.github.com>
2023-04-08 21:16:23 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,18 +20,24 @@ PASSWORD=$2
 green=$(tput setaf 2)
 lblue=$(tput setaf 6)
 reset=$(tput sgr0)
-printf " * [${green}BOOT${reset}] ${lblue}Starting bootstrap.sh${reset}...\n"
+printf "[${green}BOOT${reset}] ${lblue}Starting bootstrap.sh${reset}...\n"
 
 # Load .bashrc
-printf " * [${green}BASH${reset}] ${lblue}Loading .bashrc${reset}...\n"
+printf "[${green}BASH${reset}] ${lblue}Loading .bashrc${reset}...\n"
 
-echo "export HDFS_NAMENODE_USER=\"$1\"" >> ~/.bashrc
+FILE=.first_boot
+if [ ! -e $FILE ] ; then
+    echo "Do not remove this file" > $FILE
+    echo "export HDFS_NAMENODE_USER=\"$1\"" >> ~/.bashrc
+fi
 #source ${MYDIR}/.bashrc
 eval "$(tail -n +10 ~/.bashrc)" # workaround for ubuntu .bashrc
 
 # Copy hosts to hosts file
-printf " * [${green}HOSTS${reset}] ${lblue}Copying hosts file${reset}...\n"
-echo "${PASSWORD}" | sudo -S bash -c "cat ${PWD}/config_files/hosts > /etc/hosts"
+if [ ! -e $FILE ] ; then
+    printf "[${green}HOSTS${reset}] ${lblue}Copying hosts file${reset}...\n"
+    echo "${PASSWORD}" | sudo -S bash -c "cat ${PWD}/config_files/hosts > /etc/hosts"
+fi
 
 # Start SSH service
 echo "${PASSWORD}" | sudo -S service ssh start
@@ -54,34 +60,32 @@ if [ "$HOSTNAME" == "node-master" ] ; then
                                         
     # Time to wait for slaves to establish ssh connection with node-master
     # Only needed when node-master is started before slaves
-    sleep 6
+    sleep 5
 
     # Format HDFS only at first boot
-    FILE=.first_boot
     if [ ! -e $FILE ] ; then
-        echo "Do not remove this file" > $FILE
-        printf " * [${green}HDFS${reset}] ${lblue}Formatting filesystem${reset}...\n"
-        sleep 3
+        printf "[${green}HDFS${reset}] ${lblue}Formatting filesystem${reset}...\n"
+        sleep 2
         hdfs namenode -format
     fi
 
     # Start HDFS service
-    printf " * [${green}HDFS${reset}] ${lblue}Starting HDFS service${reset}...\n"
+    printf "[${green}HDFS${reset}] ${lblue}Starting HDFS service${reset}...\n"
     start-dfs.sh
 
-    sleep 3
-
     # Start YARN service
-    printf " * [${green}YARN${reset}] ${lblue}Starting YARN service${reset}...\n"
+    printf "[${green}YARN${reset}] ${lblue}Starting YARN service${reset}...\n"
     start-yarn.sh
 
     sleep 5
 
     # Create /user folders
-    hdfs dfs -mkdir -p /user/${HDFS_NAMENODE_USER}
-    hdfs dfs -mkdir /spark-logs
-    hdfs dfs -mkdir /spark-libs
-    hdfs dfs -put ${SPARK_HOME}/jars/*.jar /spark-libs/
+    if [ ! -e $FILE ] ; then
+        hdfs dfs -mkdir -p /user/${HDFS_NAMENODE_USER}
+        hdfs dfs -mkdir /spark-logs
+        hdfs dfs -mkdir /spark-libs
+        hdfs dfs -put ${SPARK_HOME}/jars/*.jar /spark-libs/
+    fi
 
     printf "
     ${lblue} APACHE
@@ -98,30 +102,24 @@ if [ "$HOSTNAME" == "node-master" ] ; then
     sleep 5
 
     # Start SPARK history server
-    printf " * [${green}SPARK${reset}] ${lblue}Starting SPARK history server${reset}...\n"
+    printf "[${green}SPARK${reset}] ${lblue}Starting SPARK history server${reset}...\n"
     start-history-server.sh
 
-    sleep 1
-
-    printf " * [${green}HDFS${reset}] ${lblue}Checking HDFS nodes report${reset}...\n"
+    printf "[${green}HDFS${reset}] ${lblue}Checking HDFS nodes report${reset}...\n"
     hdfs dfsadmin -report
 
-    sleep 8
+    sleep 5
 
-    printf " * [${green}YARN${reset}] ${lblue}Checking YARN nodes list${reset}...\n"
+    printf "[${green}YARN${reset}] ${lblue}Checking YARN nodes list${reset}...\n"
     yarn node -list
 
-    sleep 2
-
-    printf " * ${green}$(tput blink)All set!${reset}\n"
-else
-    sleep 2
-
-    printf " * ${green}Ready!${reset}\n"
+    printf "${green}$(tput blink)ALL SET!${reset}\n"
 fi
 
 # Cleaning up
-rm -rf config_files/
+if [ ! -e $FILE ] ; then
+    rm -rf config_files/
+fi
 
 # Start bash terminal
 /bin/bash
