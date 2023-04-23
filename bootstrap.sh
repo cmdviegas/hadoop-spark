60945f30ec90e34655000c76dba5c5b319e3b084
Carlos Viegas <cmdviegas@users.noreply.github.com>
2023-04-23 20:30:21 -0300

    v5

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,12 +9,34 @@
 # UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE, NATAL/RN
 #
 # (C) 2023 CARLOS M D VIEGAS
+# https://github.com/cmdviegas
 #
-# This is a bash script that initialize Hadoop services
+#
+### Description:
+## This is a bash script to initialize services
+#
+### How it works:
+## On first startup:
+## - Load .bashrc
+## - Start SSH server
+## - Format namenode HDFS (node-master only)
+## - Creates HDFS folders and copy files to them (node-master only)
+## - Start HDFS, YARN and HISTORY SERVER (and, if enabled, initialize HIVE schema) (node-master only)
+#
+## In the next startups:
+## - Load .bashrc
+## - Start SSH server
+## - Start HDFS, YARN and HISTORY SERVER (and, if enabled, initialize HIVE schema) (node-master only)
 #
 
-USERNAME=$1
-PASSWORD=$2
+# Some variables passed as arguments from Dockerfile
+PASSWORD=$1
+HIVEEXTERNAL=$2
+PGUSER=$3
+PGPASS=$4
+
+# Internal var to indicate that first boot has already done
+FILE=.first_boot
 
 # Some coloring
 GR=$(tput setaf 2)
@@ -24,105 +46,76 @@ SYS="[${GR}SYSTEM${RE}]${LB}"
 CLUSTER="[${GR}CLUSTER${RE}]${LB}"
 EOF="${RE}...\n"
 
-printf "${SYS} Starting bootstrap.sh${EOF}"
+# Starting this script
+printf "\n${SYS} Starting bootstrap.sh${EOF}"
 
-# Copy hosts to hosts file
-FILE=.first_boot
-if [ ! -e $FILE ] ; then
-    printf "${SYS} Copying hosts file to /etc/hosts${EOF}"
-    echo "${PASSWORD}" | sudo -S bash -c "cat ${HOME}/config_files/hosts > /etc/hosts"
-fi
-
-# Load .bashrc
-printf "\n${SYS} Loading environment variables in .bashrc${EOF}"
-#source ${MYDIR}/.bashrc
+# Loading .bashrc
+printf "${SYS} Loading environment variables in .bashrc${EOF}"
 eval "$(tail -n +10 ~/.bashrc)" # workaround for ubuntu .bashrc
 
-# Start SSH service
+# Starting SSH server
 printf "${SYS} Starting ssh server${EOF}"
 echo "${PASSWORD}" | sudo -S service ssh start
 
+# Copying .hosts to hosts file (only on first boot)
+printf "${SYS} Copying hosts file to /etc/hosts${EOF}"
+echo "${PASSWORD}" | sudo -S bash -c "cat ${HOME}/.hosts > /etc/hosts"
+
 # Do only at node-master
-if [ "$HOSTNAME" == "node-master" ] ; then
+if [ "${HOSTNAME}" == "node-master" ] ; then
 
-    # Hadoop banner
-    printf "
-    ${LB} APACHE
-    ${GR} ██░ ██  ▄▄▄      ▓█████▄  ▒█████   ▒█████   ██▓███
-    ${GR}▓██░ ██▒▒████▄    ▒██▀ ██▌▒██▒  ██▒▒██▒  ██▒▓██░  ██▒
-    ${GR}▒██▀▀██░▒██  ▀█▄  ░██   █▌▒██░  ██▒▒██░  ██▒▓██░ ██▓▒
-    ${GR}░▓█ ░██ ░██▄▄▄▄██ ░▓█▄   ▌▒██   ██░▒██   ██░▒██▄█▓▒ ▒
-    ${GR}░▓█▒░██▓ ▓█   ▓██▒░▒████▓ ░ ████▓▒░░ ████▓▒░▒██▒ ░  ░
-    ${GR} ▒ ░░▒░▒ ▒▒   ▓▒█░ ▒▒▓  ▒ ░ ▒░▒░▒░ ░ ▒░▒░▒░ ▒▓▒░ ░  ░
-    ${GR} ▒ ░▒░ ░  ▒   ▒▒ ░ ░ ▒  ▒   ░ ▒ ▒░   ░ ▒ ▒░ ░▒ ░
-    ${GR} ░  ░░ ░  ░   ▒    ░ ░  ░ ░ ░ ░ ▒  ░ ░ ░ ▒  ░░
-    ${GR} ░  ░  ░      ░  ░   ░        ░ ░      ░ ░     ${LB}$(tput blink)v3.3.5${RE}\n\n"
-                                        
-    # Time to wait for slaves to establish ssh connection with node-master
-    # Only needed when node-master is started before slaves
-    sleep 5
-
-    # Format HDFS only at first boot
-    if [ ! -e $FILE ] ; then
+    # Formatting HDFS (only on first boot)
+    if [ ! -e "${FILE}" ] ; then
         printf "${CLUSTER} Formatting filesystem${EOF}"
         hdfs namenode -format
     fi
 
     sleep 3
 
-    # Start HDFS and YARN services
+    # Starting HDFS and YARN services
     printf "${CLUSTER} Starting HDFS and YARN services${EOF}"
     start-dfs.sh && start-yarn.sh
 
     sleep 3
 
-    # Create /user folders
-    if [ ! -e $FILE ] ; then
+    # Creating /user folders inside HDFS
+    if [ ! -e "${FILE}" ] ; then
         printf "${CLUSTER} Creating folders in HDFS${EOF}"
         hdfs dfs -mkdir -p /user/${HDFS_NAMENODE_USER}
         hdfs dfs -mkdir /spark-logs
         hdfs dfs -mkdir /spark-libs
         hdfs dfs -put ${SPARK_HOME}/jars/*.jar /spark-libs/
     fi
-
-    printf "
-    ${LB} APACHE
-    ${GR}   ██████  ██▓███   ▄▄▄       ██▀███   ██ ▄█▀
-    ${GR} ▒██    ▒ ▓██░  ██▒▒████▄    ▓██ ▒ ██▒ ██▄█▒ 
-    ${GR} ░ ▓██▄   ▓██░ ██▓▒▒██  ▀█▄  ▓██ ░▄█ ▒▓███▄░ 
-    ${GR}   ▒   ██▒▒██▄█▓▒ ▒░██▄▄▄▄██ ▒██▀▀█▄  ▓██ █▄ 
-    ${GR} ▒██████▒▒▒██▒ ░  ░ ▓█   ▓██▒░██▓ ▒██▒▒██▒ █▄
-    ${GR} ▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░ ▒▒   ▓▒█░░ ▒▓ ░▒▓░▒ ▒▒ ▓▒
-    ${GR} ░ ░▒  ░ ░░▒ ░       ▒   ▒▒ ░  ░▒ ░ ▒░░ ░▒ ▒░
-    ${GR} ░  ░  ░  ░░         ░   ▒     ░░   ░ ░ ░░ ░ 
-    ${GR}       ░                 ░  ░   ░     ░  ░       ${LB}$(tput blink)v3.3.2${RE}\n\n"
-
-    # Pandas installation (optional)
-    # if [ ! -e $FILE ] ; then
-    #    echo "${PASSWORD}" | sudo apt install -y python3-pip
-    #    pip install pandas==1.5.3 pyarrow==11.0.0
-    #    echo "export PATH=\"$PATH:$HOME/.local/bin\"" >> ~/.bashrc
-    #    eval "$(tail -n +10 ~/.bashrc)" # workaround for ubuntu .bashrc
-    #fi
     
-    # Start SPARK history server
+    # Starting SPARK history server
     printf "${CLUSTER} Starting SPARK history server${EOF}"
     start-history-server.sh
 
+    # Checking HDFS status (optional)
     printf "${CLUSTER} Checking HDFS nodes report${EOF}"
     hdfs dfsadmin -report
 
+    # Checking YARN status (optional)
     printf "${CLUSTER} Checking YARN nodes list${EOF}"
     yarn node -list
+
+    # Creating hive schema for postgresql as metastore (if enabled)
+    if ${HIVEEXTERNAL}; then
+        printf "${CLUSTER} Checking hive metastore schema status${EOF}"
+        CHECKSCHEMA=`echo "$(PGPASSWORD=$PGPASS psql -h postgres-db -U $PGUSER -d hive_metastore -c '\dt "BUCKETING_COLS"')" | grep -o "1 row" | wc -l`
+        if [ $CHECKSCHEMA -eq 0 ]; then
+            printf "${CLUSTER} Initializing hive metastore schema${EOF}"
+            schematool -initSchema -dbType postgres
+        fi
+     fi
 
     printf "${CLUSTER} ${GR}$(tput blink)ALL SET!${RE}\n"
 fi
 
-# Cleaning up
-if [ ! -e $FILE ] ; then
-    echo "Do not remove this file" > $FILE
-    rm -rf config_files/
+# Creating .first_boot file as a flag to indicate that first boot has already done
+if [ ! -e "${FILE}" ] ; then
+    touch ${FILE}
 fi
 
-# Start bash terminal
+# Starting bash terminal
 /bin/bash
