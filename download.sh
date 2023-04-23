60f3053e4500e66760856088ff1d4dc676edd676
Carlos Viegas <cmdviegas@users.noreply.github.com>
2023-04-23 20:23:27 -0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -9,17 +9,24 @@
 # UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE, NATAL/RN
 #
 # (C) 2023 CARLOS M D VIEGAS
+# https://github.com/cmdviegas
 #
 # This is a bash script to automatically download hadoop and spark 
 #
 
 read -p "Download Apache Hadoop 3.3.5? (y/N): " hadoop
-read -p "Download Apache Spark 3.3.2? (y/N): " spark
+read -p "Download Apache Spark 3.4.0? (y/N): " spark
+read -p "Download Apache Hive 3.1.3? (y/N): " hive
 
 if [[ "$hadoop" =~ ^([Yy]|[Yy][Ee][Ss])$ ]]; then
   wget -nc --no-check-certificate https://dlcdn.apache.org/hadoop/common/hadoop-3.3.5/hadoop-3.3.5.tar.gz
 fi
 
 if [[ "$spark" =~ ^([Yy]|[Yy][Ee][Ss])$ ]]; then
-  wget -nc --no-check-certificate https://dlcdn.apache.org/spark/spark-3.3.2/spark-3.3.2-bin-hadoop3.tgz
+  wget -nc --no-check-certificate https://dlcdn.apache.org/spark/spark-3.4.0/spark-3.4.0-bin-hadoop3.tgz
 fi
+
+if [[ "$hive" =~ ^([Yy]|[Yy][Ee][Ss])$ ]]; then
+  wget -nc --no-check-certificate https://dlcdn.apache.org/hive/hive-3.1.3/apache-hive-3.1.3-bin.tar.gz
+fi
+
