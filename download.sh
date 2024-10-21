7f306eaaf6fc60e16d153ac121265ef4b345efa4
Carlos Viegas <cmdviegas@users.noreply.github.com>
2024-10-21 10:30:28 -0300

    Update download.sh to run in bash/sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # ██████╗  ██████╗ █████╗
 # ██╔══██╗██╔════╝██╔══██╗
 # ██║  ██║██║     ███████║
@@ -18,15 +18,20 @@ read -p "Download Apache Hadoop 3.4.0? (y/N): " hadoop
 read -p "Download Apache Spark 3.5.3? (y/N): " spark
 read -p "Download Apache Hive 3.1.3? (y/N): " hive
 
-if [[ "$hadoop" =~ ^([Yy]|[Yy][Ee][Ss])$ ]]; then
-  wget -nc --no-check-certificate https://dlcdn.apache.org/hadoop/common/hadoop-3.4.0/hadoop-3.4.0.tar.gz
-fi
+case "$hadoop" in
+  [Yy]|[Yy][Ee][Ss]) 
+    wget -nc --no-check-certificate https://dlcdn.apache.org/hadoop/common/hadoop-3.4.0/hadoop-3.4.0.tar.gz
+    ;;
+esac
 
-if [[ "$spark" =~ ^([Yy]|[Yy][Ee][Ss])$ ]]; then
-  wget -nc --no-check-certificate https://dlcdn.apache.org/spark/spark-3.5.3/spark-3.5.3-bin-hadoop3.tgz
-fi
+case "$spark" in
+  [Yy]|[Yy][Ee][Ss]) 
+    wget -nc --no-check-certificate https://dlcdn.apache.org/spark/spark-3.5.3/spark-3.5.3-bin-hadoop3.tgz
+    ;;
+esac
 
-if [[ "$hive" =~ ^([Yy]|[Yy][Ee][Ss])$ ]]; then
-  wget -nc --no-check-certificate https://dlcdn.apache.org/hive/hive-3.1.3/apache-hive-3.1.3-bin.tar.gz
-fi
-
+case "$hive" in
+  [Yy]|[Yy][Ee][Ss]) 
+    wget -nc --no-check-certificate https://dlcdn.apache.org/hive/hive-3.1.3/apache-hive-3.1.3-bin.tar.gz
+    ;;
+esac
